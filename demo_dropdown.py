f20a2590a64fed3a60a26785a41add5dbb32571f
venyutzky <venyutzky08@gmail.com>
2022-11-24 14:03:14 +0700

    menambahkan script dropdown

--- demo_dropdown.py
+++ demo_dropdown.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import time
 
 class DemoDropdown():
@@ -12,8 +13,15 @@ class DemoDropdown():
         driver.maximize_window()
         driver.implicitly_wait(10)
         driver.get("https://www.salesforce.com/ap/form/signup/freetrial-sales/?d=topnav2-btn-ft")
+        dropdwn = driver.find_element(By.NAME, "UserTitle")
+        dd = Select(dropdwn)
+        dd.select_by_index(1)
+        time.sleep(1)
+        dd.select_by_visible_text("IT Manager")
+        time.sleep(1)
+        dd.select_by_value("Others_AP")
 
 
-checkbox = DemoDropdown()
+dropdown = DemoDropdown()
 # checkbox.demo_checkbox()
-checkbox.demo_dropdown()
+dropdown.demo_dropdown()
